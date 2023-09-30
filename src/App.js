b365ca7d6fa959050832720540b95e761ef656d9
Muhammad Amsal <muhammadamsal526@gmail.com>
2023-09-30 20:47:16 +0500

    cardsflip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const allCardsData = [
+    {
+      defaultShow: "What language is React  based on",
+      hoverText: "Javascript"
+    },
+    {
+      defaultShow:"How to give components memory?",
+      hoverText:  "useState Hook"
+    },
+    {
+      defaultShow: "what's is the name of the syntax we used to describe a UI in React?",
+      hoverText: "JSX"
+    }, {
+      defaultShow: "How to pass data from parent to child components?",
+      hoverText: "Through Props"
+    }, {
+      defaultShow: "Which Language is called Object-Oriented Language?",
+      hoverText: "Javascript"
+    }, {
+      defaultShow: "Why so Javascript and Java have similar name?",
+      hoverText: "Javascript's syntax is loosely based on Java's"
+    } 
+  ];
+
+  const [cardsData,setCardsData]=useState(allCardsData)
+
+
+  function mouseOverHandler(index){
+     
+    const updatedData=[...cardsData]
+    updatedData[index].defaultShow=allCardsData[index].hoverText
+    setCardsData(updatedData)
+  }
+  function mouseLeaveHandler(){
+     
+    setCardsData(allCardsData)
+  }
+
+return (
+<>
+<div className="container" >
+<div className="wrapper" >
+
+{
+  cardsData.map((data,index)=>(
+    <div className="cards" key={index} onMouseOver={()=>{
+      mouseOverHandler(index)
+    }} onMouseLeave={()=>{
+      mouseLeaveHandler(index)
+    }} > <p>  {data.defaultShow}  </p> </div> 
+  ))
 }
+
+</div>
+
+
+</div>
+
+
+</>
+
+
+)
+
+
+
+}
+
+ 
 
 export default App;
